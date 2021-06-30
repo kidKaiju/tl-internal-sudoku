c2fce36bdebc6f7ea93c93490f21d2a1ad1c8ea8
tle <letan.87@gmail.com>
2021-06-30 23:30:44 +1000

    includes link to example

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
+from sklearn.datasets import load_digits
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split, cross_val_score
+import numpy as np
+import matplotlib.pyplot as plt 
+# https://www.toptal.com/data-science/machine-learning-number-recognition
+
+digits = load_digits()
+type(digits)
+
 print('hello')
 print('world')
